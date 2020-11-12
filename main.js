3a92d3931f33ee05a9f867955d83a6412f35358a
Maureen <maureennayebare2@gmail.com>
2020-11-12 14:22:09 +0300

    Bakery

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log('Hello World!');
+function getValue(){var value=document.getElementById('choise').value;
+    alert('You have selected ' + value + ' It can be delivered to you if you contact  077574467. Thank you');
+    
+}
+getValue();
+console.log(alerted);
